7add42d10a93e89d47338dc57eca41a713256ab9
jolinchen <jolinchen@Jolins-MacBook-Pro.local>
2022-08-18 09:28:52 +0800

    add procfile

--- chatbotFAQ/api_views.py
+++ chatbotFAQ/api_views.py
@@ -3,14 +3,12 @@ from django.http import HttpResponse
 from chatbotFAQ.forms import QuestionForm
 from .models import *
 from .serializers import *
-from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet, GenericViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("hello user!")
 
 def question_create(request):
     if request.method == 'POST':
@@ -32,9 +30,6 @@ def question_create(request):
 def question_list(request):
     all_questions = Question.objects.all()
     return Response({'All questions': all_questions})
-
-def question_get(request):
-    pass
    
 def question_sent(request):
     return HttpResponse("Your question has been sent, we will get back to you soon!")
@@ -42,30 +37,3 @@ def question_sent(request):
 class QuestionViewSet(ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-    # def question_create(request):
-    #     if request.method == 'POST':
-    #         form = QuestionForm(request.POST)
-    #         if form.is_valid():
-    #             question = form.save()
-    #             print(question)
-    #             return redirect('sent')
-    #     else:
-    #         form = QuestionForm()
-
-    #     return render(
-	# 		request,
-	# 		'simpleForm.html',
-	# 		{'form': form}
-	# 	)
-
-
-# def questions(request):
-#     if request.method == 'POST':
-#         form = ContactUsForm(request.POST)
-#         # print(form.cleaned_datas)
-#         return redirect('sent')
-#     else:
-#         form = ContactUsForm()
-#     return render(request,
-#           'simpleForm.html',
-#           {'form': form})
--- chatbotFAQ/models.py
+++ chatbotFAQ/models.py
@@ -1,7 +1,6 @@
-from cProfile import label
-from unicodedata import category
 from django.db import models
 from multiselectfield import MultiSelectField
+from django.core.validators import RegexValidator
 
 RELEVANCE_CHOICES = (
     (1, "KYC onboarding"),
@@ -13,21 +12,21 @@ RELEVANCE_CHOICES = (
     (7, "Others"),
 )
 COUNTRY_CHOICES = (
-    (1, "China"),
-    (2, "Singapore"),
-    (3, "United Kingdom"),
-    (4, "United States")
+    ("China", "China"),
+    ("Singapore", "Singapore"),
+    ("United Kingdom", "United Kingdom"),
+    ("United States", "United States")
 )
 # Create your models here.
 class Question(models.Model):
-    name = models.fields.CharField(max_length=100) 
+    name = models.fields.CharField(max_length=100, validators=[RegexValidator('[+-/%]', inverse_match=True)]) 
     category = MultiSelectField(choices=RELEVANCE_CHOICES,
                                  max_choices=1,
                                  max_length=1
                                  )
     country = MultiSelectField(choices=COUNTRY_CHOICES,
                                  max_choices=1,
-                                 max_length=1
+                                 max_length=30
                                  )
     question = models.fields.CharField(max_length=200)
     def __str__(self):
